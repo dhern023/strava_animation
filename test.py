c0ea532042a0c7fbc3753dfb5a61b927b2abb690
dhern023 <dhern023@ucr.edu>
2024-09-25 20:31:01 -0400

    POC

--- test.py
+++ test.py
@@ -2,8 +2,12 @@ import argparse
 
 parser = argparse.ArgumentParser(description = "Test program for debugging")
 parser.add_argument('--name', '-n', required=True, type=str, help = "Please enter any string" )
-parser.add_argument('--correct', '-c', type=bool, default=False, help = "A boolean flag")
+parser.add_argument('--correct', '-c', action='store_true', help = "A boolean flag")
 args = parser.parse_args()
 
-if args.correct:
-    print(args.name)
+def do_something(instance_args):
+    if instance_args.correct:
+        print(instance_args.name)
+
+if __name__ == "__main__":
+    do_something(args)
